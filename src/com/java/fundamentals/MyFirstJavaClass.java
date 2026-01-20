6bf2d8180677f19eee18ec92b6becb790cbc48fd
madhusamala-dev <madhu@trainingmug.com>
2026-01-20 16:17:53 +0530

    feat: added new class

---
+++ src/com/java/fundamentals/MyFirstJavaClass.java
@@ -0,0 +1,13 @@
+package com.java.fundamentals;
+
+public class MyFirstJavaClass {
+    public static void main(String[] args) {
+        System.out.println("My First Java Code");
+    }
+}
+
+class HelloWorld{
+    public void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
