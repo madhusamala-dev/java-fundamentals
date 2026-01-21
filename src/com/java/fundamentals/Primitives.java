8b95b59a7f7240ba627cd26936afb01a3555b991
madhusamala-dev <madhu@trainingmug.com>
2026-01-21 18:49:19 +0530

    working with primitives

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,40 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+        byte age = 35;
+        short year = 2026;
+        int salary = 234232;
+        long accountNumber = 23423423423L;
+
+        /*
+        float temperature = 36.5F;
+        float rating = 4.9F;
+        float censorValue = 3.5F;
+        */
+        float temperature, rating, censorValue;
+        temperature = 36.5F;
+        rating = 4.9F;
+        censorValue = 3.5F;
+
+        System.out.println("Age : " + age);
+
+        double annualRevenue = 2342342323.23423;
+
+        char grade = 'A';
+
+        char grade1 = 97;
+
+        System.out.println(grade1);
+
+        char ch = '1';
+        char ch1 = 1;
+
+        System.out.println(ch);
+        System.out.println(ch1);
+
+        boolean valid = true;
+
+
+    }
+}
---
+++ src/com/java/fundamentals/TakeInput.java
@@ -0,0 +1,19 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class TakeInput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte age;
+        System.out.println("Enter Your Age : ");
+        age = sc.nextByte();
+        int salary;
+        System.out.println("Enter Your Salary : ");
+        salary = sc.nextInt();
+        System.out.println("Age : " + age);
+        System.out.println("Salary : " + salary);
+        sc.close();
+    }
+}
